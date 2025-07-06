f6df652f2d39e4d95bb15f521abf31239fa52058
Cauê Garcia <cauerg7@outlook.com>
2025-07-06 15:12:15 -0300

    First axios request movies by drama

--- crg-movies-frontend/src/components/MovieCard/index.tsx
+++ crg-movies-frontend/src/components/MovieCard/index.tsx
@@ -2,26 +2,11 @@ import './styles.css';
 import GenreCategory from '../GenreCategory';
 import type { MovieDTO } from '../../models/movie';
 
-const movie: MovieDTO = {
-    id: 31,
-    name: "Meet Joe Black",
-    premiered: 1998,
-    rating: 7.8,
-    summary: "When Death personified visits Earth in the body of a young man named Joe Black, he chooses to learn about life by staying with a media mogul nearing the end of his days. As Joe unexpectedly falls in love with the man’s daughter, he begins to question his purpose. The film explores the mystery of existence, the power of love, and the inevitability of parting.",
-    image: "https://i.pinimg.com/736x/b6/23/e4/b623e444441987e98df31d75c999d5cf.jpg",
-    genres: [
-        {
-            id: 1,
-            name: "Drama"
-        },
-        {
-            id: 6,
-            name: "Romance"
-        }
-    ]
+type Props = {
+    movie: MovieDTO;
 }
 
-export default function MovieCard() {
+export default function MovieCard({ movie }: Props) {
 
     return (
         <>
--- crg-movies-frontend/src/routes/Home/CollectionPage/DramaMovies/index.tsx
+++ crg-movies-frontend/src/routes/Home/CollectionPage/DramaMovies/index.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import MovieCard from "../../../../components/MovieCard";
-
+import type { MovieDTO } from "../../../../models/movie";
+import axios from "axios";
 
 export default function DramaMovies() {
 
+    const [dramaMovie, setDramaMovie] = useState<MovieDTO[]>([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/movies/drama")
+            .then(response => {
+                setDramaMovie(response.data);
+            });
+    }, []);
+
     return (
         <section>
-            <MovieCard />
+            {
+                dramaMovie.map(
+                    dramaMovie => <MovieCard key={dramaMovie.id} movie={dramaMovie} />
+                )
+            }
         </section>
     );
 }
